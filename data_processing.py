37a191c92234e750803c7646d27d02a48ca04770
Thanutdit <thanutdit.j@ku.th>
2025-11-05 11:00:45 +0700

    initial

---
+++ data_processing.py
@@ -0,0 +1,67 @@
+import csv, os
+
+__location__ = os.path.realpath(
+    os.path.join(os.getcwd(), os.path.dirname(__file__)))
+
+cities = []
+with open(os.path.join(__location__, 'Cities.csv')) as f:
+    rows = csv.DictReader(f)
+    for r in rows:
+        cities.append(dict(r))
+
+# Print first 5 cities only
+for city in cities[:5]:
+    print(city)
+
+# Print the average temperature of all the cities
+print("The average temperature of all the cities:")
+temps = []
+for city in cities:
+    temps.append(float(city['temperature']))
+print(sum(temps)/len(temps))
+print()
+
+# Print the average temperature of all the cities
+print("The average temperature of all the cities:")
+temps = [float(city['temperature']) for city in cities]
+print(sum(temps)/len(temps))
+print()
+
+# Print all cities in Germany
+
+print("All cities in Germany:")
+temps = []
+for city in cities:
+    if city['country'] == 'Germany':
+        temps.append(city)
+print(temps)
+print()
+
+# Print all cities in Spain with a temperature above 12°C
+
+print("All cities in Spain with a temperature above 12°C:")
+for city in cities:
+    if city['country'] == 'Spain':
+        if float(city["temperature"]) > 12:
+            print(city['city'])
+
+# Count the number of unique countries
+
+'''
+Your code here
+
+'''
+
+# Print the average temperature for all the cities in Germany
+
+'''
+Your code here
+
+'''
+
+# Print the max temperature for all the cities in Italy
+
+'''
+Your code here
+
+'''
